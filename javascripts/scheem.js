3649f0db9a989d8a90cbfb4e99eab8678d1e2ce4
Lord LoriK <lord.lorik@gmail.com>
2012-05-01 02:22:08 -0300

    Tests & interactive interpreter

---
+++ javascripts/scheem.js
@@ -0,0 +1,189 @@
+scheem = (function () {
+	var parseScheem;
+	
+	if (typeof module !== 'undefined') {
+		var PEG = require('pegjs');
+		var fs = require('fs');
+		var grammar;
+
+		try {
+			grammar = fs.readFileSync('scheem.peg', 'utf-8');
+			parseScheem = PEG.buildParser().parse;
+		}
+		catch (e) {
+			grammar = fs.readFileSync('../scheem.peg', 'utf-8');
+		}
+		parseScheem = PEG.buildParser(grammar).parse;
+	}
+	else {
+		parseScheem = scheemParser.parse;
+	};
+
+	var eval = function (string, env) {
+		return evalScheem(parseScheem(string), env || {});
+	};
+
+	var isArray = function (obj) {
+		return obj && obj instanceof Array;
+	};
+
+	var checkNumber = function (obj) {
+		if (typeof obj !== 'number') throw('Invalid number');
+		return true;
+	};
+
+	var evalScheem = function (expr, env) {
+		// Special error token
+		if (expr === 'error') throw('Error');
+		
+		// Nil
+		if (expr === 'nil') return null;
+
+		// Numbers evaluate to themselves
+		if (typeof expr === 'number') {
+			return expr;
+		}
+
+		// Strings are variable references
+		if (typeof expr === 'string') {
+			return env[expr];
+		}
+
+		var len = expr.length,
+			tmp, tmp2;
+		
+		// Look at head of list for operation
+		switch (expr[0]) {
+			case '+':
+				if (len == 1) throw('Incorrect number of arguments');
+				tmp = 0;
+				for (var i = 1; i < len; ++i) {
+					tmp2 = evalScheem(expr[i], env);
+					tmp += checkNumber(tmp2) && tmp2;
+				}
+				return tmp;
+
+			case '-':
+				if (len > 3 || len < 2) throw('Incorrect number of arguments');
+				if (len == 2) return -evalScheem(expr[1], env);
+				tmp = evalScheem(expr[1], env);
+				tmp2 = evalScheem(expr[2], env);
+				return checkNumber(expr[1]) && checkNumber(expr[2]) && tmp - tmp2;
+
+			case '*':
+				if (len < 2) throw('Incorrect number of arguments');
+				tmp = 1;
+				for (var i = 1; i < len; ++i) {
+					tmp2 = evalScheem(expr[i], env);
+					tmp *= checkNumber(tmp2) && tmp2;
+				}
+				return tmp;
+
+			case '/':
+				if (len != 3) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[1], env);
+				tmp2 = evalScheem(expr[2], env);
+				if (tmp2 == 0) throw('Division by zero');
+				return checkNumber(expr[1]) && checkNumber(expr[2]) && tmp / tmp2;
+
+			case '%':
+				if (len != 3) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[1], env);
+				tmp2 = evalScheem(expr[2], env);
+				if (tmp2 == 0) throw('Division by zero');
+				return checkNumber(expr[1]) && checkNumber(expr[2]) && tmp % tmp2;
+					   
+			case 'define':
+				if (len != 3) throw('Incorrect number of arguments');
+				tmp = expr[1];
+				if (typeof tmp !== 'string' || +tmp === tmp) throw('Invalid symbol');
+				if (typeof env[tmp] !== 'undefined') throw('Symbol already defined');
+				env[tmp] = evalScheem(expr[2], env);
+				return 0;
+
+			case 'set!':
+				if (len != 3) throw('Incorrect number of arguments');
+				tmp = expr[1];
+				if (typeof env[tmp] === 'undefined') throw('Symbol not defined');
+				return env[tmp] = evalScheem(expr[2], env);
+
+			case 'begin':
+				tmp = null;
+				for (var i = 1; i < len; ++i) {
+					tmp = evalScheem(expr[i], env);
+				}
+				return tmp;
+
+			case 'quote':
+				if (len != 2) throw('Incorrect number of arguments');
+				return expr[1];
+				
+			case 'cons':
+				if (len != 3) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[2]);
+				if (!isArray(tmp)) throw('Second argument must be a list');
+				tmp = tmp.slice();
+				tmp.unshift(evalScheem(expr[1]));
+				return tmp;
+
+			case 'car':
+				if (len != 2) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[1]);
+				if (!isArray(tmp) || !tmp.length) throw('First argument must be an non-empty list');
+				return tmp[0];
+
+			case 'cdr':
+				if (len != 2) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[1]);
+				if (!isArray(tmp) || !tmp.length) throw('First argument must be a non-empty list');
+				return tmp.slice(1);
+
+			case '=':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) ===
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case '<>':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) !==
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case '<':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) <
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case '>':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) >
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case '<=':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) <=
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case '>=':
+				if (len != 3) throw('Incorrect number of arguments');
+				return (evalScheem(expr[1], env) >=
+					 evalScheem(expr[2], env)) ? '#t' : '#f';
+
+			case 'if':
+				if (len > 4 || len < 2) throw('Incorrect number of arguments');
+				tmp = evalScheem(expr[1], env);
+				if (tmp === '#t') return len > 2 ? evalScheem(expr[2], env) : '#t';
+				return len == 4 ? evalScheem(expr[3], env) : '#f';
+		}
+	};
+	
+	return {
+		eval: eval,
+		evalScheem: evalScheem
+	}
+})();
+
+// If we are used as Node module, export evalScheem
+if (typeof module !== 'undefined') {
+	module.exports.eval = scheem.eval;
+	module.exports.evalScheem = scheem.evalScheem;
+}
---
+++ javascripts/tests.js
@@ -0,0 +1,508 @@
+var parseScheem, evalScheem, evalScheemString, assert, expect;
+
+if (typeof module !== 'undefined') {
+    // In Node load required modules
+	var chai = require('chai');
+	var mocha = require('mocha');
+	
+    assert = chai.assert;
+    expect = chai.expect;
+
+    var PEG = require('pegjs');
+    var fs = require('fs');
+	var scheem = require('../scheem');
+	
+	try {
+		parseScheem = PEG.buildParser(fs.readFileSync('scheem.peg', 'utf-8')).parse;
+	}
+	catch (e) {
+		parseScheem = PEG.buildParser(fs.readFileSync('../scheem.peg', 'utf-8')).parse;
+	}
+    evalScheem = scheem.evalScheem;
+    evalScheemString = scheem.eval;
+}
+else {
+    // In browser assume already loaded by <script> tags
+    assert = chai.assert;
+    expect = chai.expect;
+
+    evalScheem = scheem.evalScheem;
+    evalScheemString = scheem.eval;
+    parseScheem = scheemParser.parse;
+}
+
+suite('quote', function () {
+    test('a number', function () {
+        assert.deepEqual(
+            evalScheem(['quote', 3]),
+            3
+        );
+    });
+    test('an atom', function () {
+        assert.deepEqual(
+            evalScheem(['quote', 'dog']),
+            'dog'
+        );
+    });
+    test('a list', function () {
+        assert.deepEqual(
+            evalScheem(['quote', [1, 2, 3]]),
+            [1, 2, 3]
+        );
+    });
+    test('too many parameters', function () {
+        expect(function () {
+            evalScheem(['quote', [1], 2]);
+        }).to.throw();
+    });
+});
+
+suite('cons', function () {
+    test('a number', function () {
+        assert.deepEqual(
+            evalScheem(['cons', 1, ['quote', [2, 3]]]),
+            [1, 2, 3]
+        );
+    });
+    test('a list', function () {
+        assert.deepEqual(
+            evalScheem(['cons', ['quote', [1, 2]], ['quote', [2, 3]]]),
+            [[1, 2], 2, 3]
+        );
+    });
+});
+
+suite('car', function () {
+    test('a list', function () {
+        assert.deepEqual(
+            evalScheem(['car', ['quote', [[2, 3], 3, 4]]]),
+            [2, 3]
+        );
+    });
+    test('a number', function () {
+        assert.deepEqual(
+            evalScheem(['car', ['quote', [1, 2]]]),
+            1
+        );
+    });
+    test('empty list', function () {
+        expect(function () {
+            evalScheem(['car', ['quote', []]]);
+        }).to.throw();
+    });
+    test('too many parameters', function () {
+        expect(function () {
+            evalScheem(['car', ['quote', [1, 2]], 3]);
+        }).to.throw();
+    });
+    test('non list', function () {
+        expect(function () {
+            evalScheem(['car', 1]);
+        }).to.throw();
+    });
+});
+
+suite('cdr', function () {
+    test('a list', function () {
+        assert.deepEqual(
+            evalScheem(['cdr', ['quote', [1, 3, 4]]]),
+            [3, 4]
+        );
+    });
+    test('a single element list', function () {
+        assert.deepEqual(
+            evalScheem(['cdr', ['quote', [1]]]),
+            []
+        );
+    });
+    test('an empty list', function () {
+        expect(function () {
+            evalScheem(['cdr', ['quote', []]]);
+        }).to.throw();
+    });
+    test('too many parameters', function () {
+        expect(function () {
+            evalScheem(['cdr', ['quote', [1, 2]], 3]);
+        }).to.throw();
+    });
+    test('non list', function () {
+        expect(function () {
+            evalScheem(['cdr', 3]);
+        }).to.throw();
+    });
+});
+
+suite('define', function () {
+    test('define', function () {
+        var env = { b: 1 };
+		
+        evalScheem(['define', 'a', 3], env);
+        assert.deepEqual(
+            env,
+            { a: 3, b: 1 }
+        );
+    });
+    test('define already defined', function () {
+        expect(function () {
+            evalScheem(['define', 'a', 3], { a: 5 });
+        }).to.throw();
+    });
+    test('define too many parameters', function () {
+        expect(function () {
+            evalScheem(['define', 'a', 3, 4]);
+        }).to.throw();
+    });
+    test('define a number', function () {
+        expect(function () {
+            evalScheem(['define', '5', 3]);
+        }).to.throw();
+    });
+});
+
+suite('set!', function () {
+    test('set!', function () {
+        var env = { a: 4, b: 1 };
+		
+        evalScheem(['set!', 'a', 3], env);
+        assert.deepEqual(
+			env,
+            { a: 3, b: 1 }
+        );
+    });
+    test('set! too many parameters', function () {
+        expect(function () {
+            evalScheem(['set!', 'a', 3, 4]);
+        }).to.throw();
+    });
+    test('set! not yet defined', function () {
+        expect(function () {
+            evalScheem(['set!', 'a', 3]);
+        }).to.throw();
+    });
+    test('set! expression', function () {
+        var env = { a: 4, b: 1 };
+		
+        evalScheem(['set!', 'a', ['+', 1, 2]], env);
+        assert.deepEqual(
+			env,
+            { a: 3, b: 1 }
+        );
+    });
+});
+
+suite('begin', function () {
+    test('a number', function () {
+        assert.deepEqual(
+            evalScheem(['begin', 1, 2, 3]),
+            3
+        );
+    });
+    test('an expression', function () {
+        assert.deepEqual(
+            evalScheem(['begin', 1, 2, ['+', 3, 4]]),
+            7
+        );
+    });
+    test('change environment', function () {
+        var env = { a: 4 };
+		
+        evalScheem(['begin', ['set!', 'a', 3]], env);
+        assert.deepEqual(
+            env,
+            { a: 3 }
+        );
+    });
+    test('track environment', function () {
+        var env = { a: 4 };
+		
+        assert.deepEqual(
+			evalScheem(['begin', ['set!', 'a', 3], ['+', 'a', 2]], env),
+            5
+        );
+    });
+});
+
+suite('if', function () {
+    test('cond true, two branches', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['=', 1, 1], 2, 3]),
+            2
+        );
+    });
+    test('cond false, two branches', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['<>', 1, 1], 2, 3]),
+            3
+        );
+    });
+    test('cond true, one branch', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['=', 1, 1], 2]),
+            2
+        );
+    });
+    test('cond false, one branch', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['<>', 1, 1], 2]),
+            '#f'
+        );
+    });
+    test('cond true, no branches', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['=', 1, 1]]),
+            '#t'
+        );
+    });
+    test('cond false, no branches', function () {
+        assert.deepEqual(
+            evalScheem(['if', ['<>', 1, 1]]),
+            '#f'
+        );
+    });
+});
+
+suite('math', function () {
+    test('add', function () {
+        assert.deepEqual(
+            evalScheem(['+', 1, 2]),
+            3
+        );
+    });
+    test('add single', function () {
+        assert.deepEqual(
+            evalScheem(['+', 1]),
+            1
+        );
+    });
+    test('add multiple', function () {
+        assert.deepEqual(
+            evalScheem(['+', 1, 2, 3]),
+            6
+        );
+    });
+    test('add multiple', function () {
+        assert.deepEqual(
+            evalScheem(['+', 1, 2, 3]),
+            6
+        );
+    });
+    test('subtract', function () {
+        assert.deepEqual(
+            evalScheem(['-', 1, 2]),
+            -1 
+        );
+    });
+    test('unary minus', function () {
+        assert.deepEqual(
+            evalScheem(['-', 1]),
+            -1
+        );
+    });
+    test('multiply', function () {
+        assert.deepEqual(
+            evalScheem(['*', 3, 2]),
+            6 
+        );
+    });
+    test('multiply single', function () {
+        assert.deepEqual(
+            evalScheem(['*', 3]),
+            3
+        );
+    });
+    test('multiply multiple', function () {
+        assert.deepEqual(
+            evalScheem(['*', 2, 3, 4]),
+            24
+        );
+    });
+    test('divide', function () {
+        assert.deepEqual(
+            evalScheem(['/', 12, 3]),
+            4 
+        );
+    });
+    test('modulus', function () {
+        assert.deepEqual(
+            evalScheem(['%', 7, 3]),
+            1
+        );
+    });
+});
+
+suite('comparison', function () {
+    test('equal', function () {
+        assert.deepEqual(
+            evalScheem(['=', 2, 2]),
+            '#t'
+        );
+    });
+    test('not equal', function () {
+        assert.deepEqual(
+            evalScheem(['=', 1, 2]),
+            '#f'
+        );
+    });
+    test('not-equal', function () {
+        assert.deepEqual(
+            evalScheem(['<>', 2, 2]),
+            '#f'
+        );
+    });
+    test('not not-equal', function () {
+        assert.deepEqual(
+            evalScheem(['<>', 1, 2]),
+            '#t'
+        );
+    });
+    test('less than', function () {
+        assert.deepEqual(
+            evalScheem(['<', 2, 3]),
+            '#t' 
+        );
+    });
+    test('not less than', function () {
+        assert.deepEqual(
+            evalScheem(['<', 12, 3]),
+            '#f' 
+        );
+    });
+    test('less than or equal to, less', function () {
+        assert.deepEqual(
+            evalScheem(['<=', 4, 12]),
+            '#t' 
+        );
+    });
+    test('less than or equal to, equal', function () {
+        assert.deepEqual(
+            evalScheem(['<=', 12, 12]),
+            '#t' 
+        );
+    });
+    test('not less than or equal to', function () {
+        assert.deepEqual(
+            evalScheem(['<=', 3, 2]),
+            '#f' 
+        );
+    });
+    test('greater than', function () {
+        assert.deepEqual(
+            evalScheem(['>', 12, 3]),
+            '#t' 
+        );
+    });
+    test('not greater than', function () {
+        assert.deepEqual(
+            evalScheem(['>', 2, 3]),
+            '#f' 
+        );
+    });
+    test('greater than or equal to, greater', function () {
+        assert.deepEqual(
+            evalScheem(['>=', 12, 4]),
+            '#t' 
+        );
+    });
+    test('greater than or equal to, equal', function () {
+        assert.deepEqual(
+            evalScheem(['>=', 12, 12]),
+            '#t' 
+        );
+    });
+    test('not greater than or equal to', function () {
+        assert.deepEqual(
+            evalScheem(['>=', 2, 3]),
+            '#f' 
+        );
+    });
+});
+
+suite('parse', function () {
+    test('alphanumeric atoms', function () {
+        assert.deepEqual(
+            parseScheem('a'),
+            'a'
+        );
+    });
+    test('special char atom', function () {
+        assert.deepEqual(
+            parseScheem('!'),
+            '!'
+        );
+    });
+    test('numeric atom', function () {
+        assert.deepEqual(
+            parseScheem('9876543210'),
+            9876543210
+        );
+    });
+    test('flat list, empty', function () {
+        assert.deepEqual(
+            parseScheem('()'),
+            []
+        );
+    });
+    test('flat list, one element', function () {
+        assert.deepEqual(
+            parseScheem('(a)'),
+            ['a']
+        );
+    });
+    test('flat list, many elements', function () {
+        assert.deepEqual(
+            parseScheem('(a b c)'),
+            ['a', 'b', 'c']
+        );
+    });
+    test('nested list, single element', function () {
+        assert.deepEqual(
+            parseScheem('((a))'),
+            [['a']]
+        );
+    });
+    test('nested list, complex', function () {
+        assert.deepEqual(
+            parseScheem('(a b (c d e (f g)))'),
+            ['a', 'b', ['c', 'd', 'e', ['f', 'g']]]
+        );
+    });
+    test('quote', function () {
+        assert.deepEqual(
+            parseScheem('\'(b d)'),
+            ['quote', ['b', 'd']]
+        );
+    });
+    test('whitespace', function () {
+        assert.deepEqual(
+            parseScheem('  (\n\ra\n\tb\n\t c\r)\t'),
+            ['a', 'b', 'c']
+        );
+    });
+    test('comments', function () {
+        assert.deepEqual(
+            parseScheem('(a b;;comment with closing parenthesis)\n)'),
+            ['a', 'b']
+        );
+    });
+});
+
+suite('evaluation', function () {
+    test('add', function () {
+        assert.deepEqual(
+            evalScheemString('(+ 1 2)'),
+            3
+        );
+    });
+    test('begin', function () {
+        assert.deepEqual(
+            evalScheemString('(begin (define x 5) (+ x 2))'),
+            7
+        );
+    });
+    test('if', function () {
+        assert.deepEqual(
+            evalScheemString('(if (= 3 3) \'(x y) \'(z w))'),
+            ['x', 'y']
+        );
+    });
+});
